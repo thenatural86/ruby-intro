02eb32b33eab5aa96fa82757e662e902a193fd92
thenatural86 <philip.smalls86@gmail.com>
2021-05-26 17:00:39 -0400

    strings and interpolation

---
+++ strings.rb
@@ -0,0 +1,7 @@
+sentence = 'my name is Philip'
+first_name = 'Philip'
+last_name = 'Smalls'
+
+puts sentence
+puts first_name + " " + last_name
+puts "My first name is #{first_name} and my last name is #{last_name}."
