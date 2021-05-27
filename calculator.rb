dac6e3602209224401d7ca271556f50952fa4825
thenatural86 <philip.smalls86@gmail.com>
2021-05-27 11:44:38 -0400

    calc

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,8 @@ puts "Enter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
-puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
 puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
-puts "The first number added to the second number is #{num_1.to_i + num_2.to_i}"
-puts "The first number minus the second number is #{num_1.to_i - num_2.to_i}"
+puts "The first number added to the second number is #{num_1.to_f + num_2.to_f}"
+puts "The first number minus the second number is #{num_1.to_f - num_2.to_f}"
+puts "The first number mod the second number is #{num_1.to_f % num_2.to_f}"
