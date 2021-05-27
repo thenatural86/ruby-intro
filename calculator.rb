6b68b192c38ae0a2f5363add23f9afaad140f74c
thenatural86 <philip.smalls86@gmail.com>
2021-05-27 12:27:57 -0400

    calc methods

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,23 @@
+def multiply(num_1, num_2)
+  num_1.to_f * num_2.to_f
+end
+
+def divide(num_1, num_2)
+  num_1.to_f / num_2.to_f
+end
+
+def add(num_1, num_2)
+  num_1.to_f + num_2.to_f
+end
+
+def subtract(num_1, num_2)
+  num_1.to_f - num_2.to_f
+end
+
+def mod(num_1, num_2)
+  num_1.to_f % num_2.to_f
+end
+
 puts "Simple calculator"
 25.times {print "-"}
 puts
@@ -5,8 +25,11 @@ puts "Enter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
-puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
-puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
-puts "The first number added to the second number is #{num_1.to_f + num_2.to_f}"
-puts "The first number minus the second number is #{num_1.to_f - num_2.to_f}"
-puts "The first number mod the second number is #{num_1.to_f % num_2.to_f}"
+puts "The first number multiplied by the second number is #{multiply(num_1, num_2)}"
+puts "The first number divided by the second number is #{divide(num_1,num_2)}"
+puts "The first number added to the second number is #{add(num_1,num_2)}"
+puts "The first number minus the second number is #{subtract(num_1,num_2)}"
+puts "The first number mod the second number is #{mod(num_1,num_2)}"
+
+
+
