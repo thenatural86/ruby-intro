b0c9c041a2d512fffe82f825ba67bb1a85321cdb
thenatural86 <philip.smalls86@gmail.com>
2021-05-27 11:30:45 -0400

    simple calculator output

---
+++ calculator.rb
@@ -0,0 +1,11 @@
+puts "Simple calculator"
+25.times {print "-"}
+puts
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
+puts "The first number added to the second number is #{num_1.to_i + num_2.to_i}"
+puts "The first number minus the second number is #{num_1.to_i - num_2.to_i}"
