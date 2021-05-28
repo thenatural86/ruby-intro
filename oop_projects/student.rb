23a0ef69e79eb05da176d4be30c80576eb80c9b1
thenatural86 <philip.smalls86@gmail.com>
2021-05-28 16:27:39 -0400

    intro to oop

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -1,3 +1,16 @@
 class Student
+  @first_name
+  @last_name
+  @email
+  @ussername
+  @password
+  
+  def to_s
+    "First name: #{@first_name}" 
+  end
   
 end
+
+philip = Student.new
+puts philip
+# philip.first_name = "Philip"
