4bde9bdd069df3ecc93823979374b2ed6e392dfa
thenatural86 <philip.smalls86@gmail.com>
2021-05-28 16:58:39 -0400

    initialize object

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -1,16 +1,22 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @ussername
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
+  
+  def initialize(firstname, lastname, email, username, password)
+    @first_name = firstname
+    @last_name = lastname
+    @email = email
+    @username = username
+    @password = password
+  end
   
   def to_s
-    "First name: #{@first_name}" 
+    "First name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Username: #{@username}, Password: #{@password}" 
   end
   
 end
 
-philip = Student.new
+philip = Student.new("Philip", "Smalls", "Philip@smalls.com", "SmallsBoy69", "YOLO")
+joe = Student.new("Joe", "Smoe", "Joe@smoe.com", "JoeStarBoy72", "MOLLY")
+
 puts philip
-# philip.first_name = "Philip"
+puts joe
