d6bc8a28fdd6d98dc802c2a78de4828e062ede99
thenatural86 <philip.smalls86@gmail.com>
2021-05-28 11:40:16 -0400

    while loop

--- auth.rb
+++ auth.rb
@@ -1,7 +1,7 @@
 users = [
-  {username: "tom", password: "tom"},
-  {username: "joe", password: "joe"},
-  {username: "mac", password: "mac"},
+  {username: "tom", password: "pass1"},
+  {username: "joe", password: "pass2"},
+  {username: "mac", password: "pass3"},
 ]
 
 puts "Welcome to the authenticator"
@@ -9,3 +9,34 @@ puts "Welcome to the authenticator"
 puts
 puts "This program will take input from the user and compare the password"
 puts "If the password is correct, you will get back the user object"
+
+
+attempts = 1
+while attempts < 4
+  puts "keep running"
+  attempts += 1
+end
+
+# print "Username: "
+# name = gets.chomp
+# print "Password: "
+# pass = gets.chomp
+
+
+# input = gets.chomp
+
+
+
+# users.each do |user| 
+#   correct_name = user[:username]
+#   correact_pass = user[:password]
+  
+#   if(name == correct_name && pass == correact_pass)
+#     puts user
+#   else
+#     puts "Credentials were not correct"
+#     puts "Press n to quit or any other key to continue:"
+#   end
+# end
+
+
